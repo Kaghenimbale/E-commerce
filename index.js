3865191889672c298cc551d480fcfb6e352a44bc
Ludovic Athanas <116667804+Ludovic28@users.noreply.github.com>
2022-11-25 17:31:48 +0400

    Finished design lightbox for the desktop version

---
+++ index.js
@@ -0,0 +1,16 @@
+// const imageThumbnails = document.getElementsByClassName(".image-thumbnails");
+// const imageProduct1Desktop = document.querySelector(".product-image1-desktop");
+// const lightboxBackground = document.createElement("div");
+
+// lightboxBackground.style.backgroundColor = "black";
+// lightboxBackground.style.opacity = "40%";
+// lightboxBackground.style.width = "100vw";
+// lightboxBackground.style.height = "100vh";
+// lightboxBackground.style.position = "fixed";
+// // @ts-ignore
+// lightboxBackground.append(imageThumbnails);
+// imageProduct1Desktop.addEventListener("click", () => {
+//   const html = document.querySelector("html");
+
+//   html.appendChild(lightboxBackground);
+// });
